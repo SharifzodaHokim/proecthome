0422871a296a7f6ca8ac903b14f45f1a3879daf4
hokim <hokimsharifzoda378@gmail.com>
2024-07-29 15:06:36 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child:  Scaffold(
+        body: 
+        bottom: TabBar(
+          tabs:[
+
+          ],
+        ),
+      ),
+      
+    );
+  }
+}
